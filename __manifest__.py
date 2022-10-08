e2bdda6a08b7ad196e87460552b7d98131c2003b
Isco170 <franciscodamana@gmail.com>
2022-10-08 11:07:57 +0200

    Base

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+ 'name': "Biblioteca",
+  'summary': "Gerenciar facilmente livros",
+  'description':"Descricao",
+  'author': "Francisco André Mavie",
+  'website': "https://github.com/Isco170/Isco170",
+  'category': "Tools",
+  'version': '14.0.1',
+  'depends': ['base'],
+  'data': [
+    'security/groups.xml',
+    'security/ir.model.access.csv',
+    'views/biblioteca_livro.xml'
+  ],
+#   'demo': ['demo.xml'],
+}
