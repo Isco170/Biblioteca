73b872ea25c83b7c578ca90fb5070bd6400e6f0d
Isco170 <franciscodamana@gmail.com>
2022-10-12 13:40:56 +0200

    Computed field

--- models/biblioteca_livro.py
+++ models/biblioteca_livro.py
@@ -1,4 +1,6 @@
-from odoo import models, fields
+from datetime import timedelta
+from odoo import models, fields, api
+
 class BibliotecaLivro(models.Model):
   _name = 'biblioteca.livro'
   _description = 'Biblioteca Livro'
@@ -18,6 +20,14 @@ class BibliotecaLivro(models.Model):
   date_release = fields.Date('Release Date')
   date_updated = fields.Datetime('Last Updated')
   pages = fields.Integer('Number of Pages')
+  age_days = fields.Float(
+    string = 'Days Since Release',
+    compute = '_compute_age',
+    inverse = '_inverse_age',
+    search ='search_age',
+    store = False, # optional
+    compute_sudo = True # optional
+  )
   reader_rating = fields.Float(
     'Reader Avarege Rating', 
     digits = (14, 4),
@@ -48,10 +58,8 @@ class BibliotecaLivro(models.Model):
   category_id = fields.Many2one('biblioteca.livro.categoria')
   
   _sql_constraints = [
-    ('name_uniq', 'UNIQUE (name)',
-      'O tituli do livro deve ser unico.'),
-    ('positive_page', 'CHECK(pages>0)',
-      'Nr de paginas deve ser positivo')
+    ('name_uniq', 'UNIQUE (name)','O titulo do livro deve ser unico.'),
+    ('positive_page', 'CHECK(pages>0)','Nr de paginas deve ser positivo')
   ]
   
   @api.constrains('date_release')
@@ -60,6 +68,36 @@ class BibliotecaLivro(models.Model):
       if record.date_release and record.date_release > fields.Date.today():
         raise models.ValidationError(
         'Release date must be in the past')
+  
+  def _inverse_age(self):
+    today = fields.Date.today()
+    for book in self.filtered('date_release'):
+      d = today = timedelta(days=book.age_days)
+      bool.date_release = d
+  
+  def _search_age(self, operator, value):
+    today = fields.Date.today()
+    value_days = timedelta(days=value)
+    value_date = today - value_days
+#     Convert the operator:
+# book with age > value have a date <value_date
+    operator_map = {
+      '>': '<', '>=': '<=',
+      '<': '>', '<=': '>=',
+    }
+    
+    new_op = operator_map.get(operator, operator)
+    return [('date_release',new_op, value_date)]
+  
+  @api.depends('date_release')
+  def _compute_age(self):
+    today = fields.Date.today()
+    for book in self:
+      if book.date_release:
+        delta = today - book.date_release
+        book.age_days = delta.days
+      else:
+        book.age_days = 0
   
 class ResPartner(models.Model):
   _inherit = 'res.partner'
