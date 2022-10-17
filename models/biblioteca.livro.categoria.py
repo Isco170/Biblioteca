1ab1bc23dd5b127dbff74274ca0161c7966946c5
Isco170 <franciscodamana@gmail.com>
2022-10-17 08:43:07 +0200

    Creating new records - adicionar button na view

--- models/biblioteca.livro.categoria.py
+++ models/biblioteca.livro.categoria.py
@@ -31,3 +31,18 @@ class LivroCategoria(models.Model):
       'name': 'Child category 1',
       'description': 'Description for child 1'
     }
+    categ2 = {
+      'name': 'Child category 2',
+      'description': 'Description for child 2'
+    }
+    
+    parent_category_val = {
+      'name': 'Parent category',
+      'email': 'Description for parent category'
+      'childs_id': [
+        (0, 0, categ1),
+        (0, 0, categ2),
+      ]
+    }
+    
+    record = self.env['biblioteca.livro.categoria'].create(parent_category_val)
