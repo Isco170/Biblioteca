1976a546c2bea10323f7da4fe8e44b96185c27cf
Isco170 <franciscodamana@gmail.com>
2022-10-18 14:05:57 +0200

    Extending the business logic defined in a model

--- Biblioteca_retornos/models/biblioteca_retorno.py
+++ Biblioteca_retornos/models/biblioteca_retorno.py
@@ -1,26 +1,23 @@
-<?xml version="1.0" encoding="utf-8" ?>
-<odoo>
-<!--  Book Form View inherit  -->
-  <record id="biblioteca_livro_view_form_inh" model="ir.ui.view">
-    <field name="name">Biblioteca Livro Form</field>
-    <field name="model">biblioteca.livro</field>
-    <field name="inherit_id" ref="biblioteca.biblioteca_livro_view_form"/>
-    <field name="arch" type="xml">
-      <field name="author_ids" position="after">
-        <field name="date_return"/>
-      </field>
-    </field>
-  </record>
+from odoo import models, fields, api
+
+class BibliotecaLivro(models.Model):
+  _inherit = 'biblioteca.livro'
   
-<!--  Book Category Form View inherit  -->
-  <record id="biblioteca_livro_categoria_view_form_inh" model="ir.ui.view">
-    <field name="name">Biblioteca Categoria Form</field>
-    <field name="model">biblioteca.livro.categoria</field>
-    <field name="inherit_id" ref="biblioteca.biblioteca_livro_categoria" />
-    <field name="arch" type="xml">
-      <field name="parent_id" position="after">
-        <field name="max_borrow_days" />
-      </field>
-    </field>
-  </record>
-</odoo>
+  date_return = fields.Date('Data retorno')
+  
+  def make_borrowed(self):
+    day_to_borrow = self.categoria_id.max_borrow_days or 10
+    self.date_return = fields.Date.today() + timedelta(days = day_to_borrow)
+    return super(BibliotecaLivro, self).make_borrowed()
+  
+  def make_available(self):
+    self.date_return = False
+    return super(BibliotecaLivra, self).make_available()
+  
+class BibliotecaLivroCategoria(models.Model):
+  _inherit = 'biblioteca.livro.categoria'
+  
+  max_borrow_days = fields.Integer(
+    'Maximo dias emprestado',
+    help = "Por quantos dias o livro pode ser emprestado",
+    default = 10)
