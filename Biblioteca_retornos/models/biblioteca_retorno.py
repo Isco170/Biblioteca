c671d8b8b9df74aa4cf3bb5dcc91a633de5fe8aa
Isco170 <franciscodamana@gmail.com>
2022-10-18 09:25:39 +0200

    Extending the business logic defined in a model - View 2

--- Biblioteca_retornos/models/biblioteca_retorno.py
+++ Biblioteca_retornos/models/biblioteca_retorno.py
@@ -1,14 +1,26 @@
-from odoo import models, fields, api
-
-class BibliotecaLivro(models.Model):
-  _inherit = 'biblioteca.livro'
+<?xml version="1.0" encoding="utf-8" ?>
+<odoo>
+<!--  Book Form View inherit  -->
+  <record id="biblioteca_livro_view_form_inh" model="ir.ui.view">
+    <field name="name">Biblioteca Livro Form</field>
+    <field name="model">biblioteca.livro</field>
+    <field name="inherit_id" ref="biblioteca.biblioteca_livro_view_form"/>
+    <field name="arch" type="xml">
+      <field name="author_ids" position="after">
+        <field name="date_return"/>
+      </field>
+    </field>
+  </record>
   
-  date_return = fields.Date('Data retorno')
-  
-class BibliotecaLivroCategoria(models.Model):
-  _inherit = 'biblioteca.livro.categoria'
-  
-  max_borrow_days = fields.Integer(
-    'Maximo dias emprestado',
-    help = "Por quantos dias o livro pode ser emprestado",
-    default = 10)
+<!--  Book Category Form View inherit  -->
+  <record id="biblioteca_livro_categoria_view_form_inh" model="ir.ui.view">
+    <field name="name">Biblioteca Categoria Form</field>
+    <field name="model">biblioteca.livro.categoria</field>
+    <field name="inherit_id" ref="biblioteca.biblioteca_livro_categoria" />
+    <field name="arch" type="xml">
+      <field name="parent_id" position="after">
+        <field name="max_borrow_days" />
+      </field>
+    </field>
+  </record>
+</odoo>
